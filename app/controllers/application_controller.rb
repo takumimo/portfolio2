2b4b0bdbddbfd7b9e9ba9fceffb12a97b1c0e83c
vagrant <vagrant@localhost.localdomain>
2020-10-11 08:56:36 +0000

    update_activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,18 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource)
-		posts_index_path
+	  case resource
+      when User
+        #User(一般ユーザ)がログインした時の処理
+      when AdminUser
+        #AdminUser（管理ユーザ）がログインしたときの処理
+        stored_location_for(resource) ||
+         if resource.is_a?(AdminUser)
+           root_path
+         else
+           super
+         end
+      end
 	end
 
 	def after_sign_out_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'post_comments/create'
   get 'post_comments/destroy'
   get 'posts/index'
