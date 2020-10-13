ce1b0590fe104ffea0a6b98baad5189fe045457d
vagrant <vagrant@localhost.localdomain>
2020-10-13 13:15:53 +0000

    update_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,31 @@
 class PostsController < ApplicationController
   def index
+  	@posts = current_user.posts
   end
+
+  def new
+  	@post = Post.new
+  end
+
+  def confirm
+
+  	@post = Post.new
+  	@post.text = params[:post][:text]
+  	@post.valid?
+  	redirect_to new_post_path
+  	
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	@post.save
+  	redirect_to posts_path
+  end
+
+  private
+  	def post_params
+  	 params.require(:post).permit(:user_id, :text)
+  	end
+
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+	belongs_to :user, optional: true
+	validates :text, presence: true, length: { maximum: 3 }
 end
