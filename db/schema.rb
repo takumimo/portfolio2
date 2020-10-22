5b15da68c535403f9613736bf651368b768f402f
vagrant <vagrant@localhost.localdomain>
2020-10-22 05:20:20 +0000

    [note] 投稿機能のレイアウト変更ã

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_025212) do
+ActiveRecord::Schema.define(version: 2020_10_22_021900) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_025212) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "posts_tags", force: :cascade do |t|
