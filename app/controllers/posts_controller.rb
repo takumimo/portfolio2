82872b3d27b3ba37aa46d6156d5ce51eabf857bb
vagrant <vagrant@localhost.localdomain>
2020-10-24 09:16:04 +0000

    success tag's association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,9 @@ class PostsController < ApplicationController
 
   def index
   	@posts = Post.all
+    if params[:tag_name]
+      @posts = Post.tagged_with("#{params[:tag_name]}")
+    end
   end
 
   def show
