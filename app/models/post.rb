7302d78c37f69165a23a9f0c152f124c5eddd5d9
vagrant <vagrant@localhost.localdomain>
2020-10-24 07:40:26 +0000

    delete post_tag.rb and modify post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@ class Post < ApplicationRecord
 	belongs_to :user, optional: true
 	has_many :post_comments, dependent: :destroy
 	has_many :likes, dependent: :destroy
-
-	has_many :tags, dependent: :destroy
 	validates :text, presence: true, length: { maximum: 30 }
 	
 	def liked_by?(user)
