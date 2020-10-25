7eb2904160f7807c8e2e8eb1b881685b1e8db3a3
vagrant <vagrant@localhost.localdomain>
2020-10-25 01:40:30 +0000

    add the function of profile_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  attachment :profile_image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '48f23851dfa830f4709c5a4b9eb4484ac64618745fe98912ab4bccfbfc6746cced8dceeeafe8098256c9937f27565c4661eb4bad09371e63c7bf107c8f2456d5'
