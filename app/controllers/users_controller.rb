4e0aa60b32726af5048a51bef4d71270b7bc71b2
vagrant <vagrant@localhost.localdomain>
2020-10-30 05:57:28 +0000

    add follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,14 @@ class UsersController < ApplicationController
   	end
   end
 
+  def following
+      @users = User.find(params[:id]).following
+  end
+
+  def followers
+      @users = User.find(params[:id]).followers
+  end
+
   private
   def user_params
   	params.require(:user).permit(:email, :name, :introduction, :profile_image)
