e324fefdf8535cc8cd9175b308d8ba6fe72fbf4b
motokitakumi <mtktkm242424@gmail.com>
2020-11-03 15:55:16 +0900

    add thanks

--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -1,2 +1,19 @@
 class ThanksController < ApplicationController
+
+
+	def create
+		@post_comment = PostComment.find(params[:post_comment_id])
+	    thank = @post_comment.thanks.new(user_id: current_user.id)
+		thank.save
+		redirect_to request.referrer
+	end
+
+	def destroy
+		@post_comment = PostComment.find(params[:post_comment_id])
+	    thank = current_user.thanks.find_by(post_comment_id: @post_comment.id)
+	    thank.destroy
+	    redirect_to request.referrer
+	end
+
+	
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,4 +2,8 @@ class PostComment < ApplicationRecord
 	belongs_to :post
 	belongs_to :user
 	has_many :thanks, dependent: :destroy
+
+	def thanked_by?(user)
+    	thanks.where(user_id: user.id).exists?
+  	end
 end
