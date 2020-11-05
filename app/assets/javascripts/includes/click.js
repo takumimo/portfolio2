09ff7f1789a887cb16d90b054451d8e808c247c8
vagrant <vagrant@localhost.localdomain>
2020-11-05 10:24:17 +0000

    fix thanks layout

--- app/assets/javascripts/includes/click.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("mypage").onclick = function(){
-  const HeaderDropdown = document.getElementById("dropdown");
-  if(HeaderDropdown.style.display=="block"){
-    HeaderDropdown.style.display = "none";
-  }else{
-    HeaderDropdown.style.display = "block";
-  };
-};
