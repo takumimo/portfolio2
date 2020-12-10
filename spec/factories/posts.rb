2e22619046fd872f5e9c749b0b802f2936c4e3c0
vagrant <vagrant@localhost.localdomain>
2020-12-10 02:40:50 +0000

    add faker emoji and tag_list

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :post do
     title { Faker::Lorem.characters(number: 10) }
     text { Faker::Lorem.characters(number: 50) }
+    emoji { Faker::Lorem.characters(number: 1) }
+    tag_list { Faker::Lorem.characters(number: 5) }
     user
   end
 end
