2b9fcf617f2aab8d6e3f82d6f68eabbb8420e7b5
vagrant <vagrant@localhost.localdomain>
2020-12-14 13:53:32 +0000

    fix several bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,9 +30,9 @@ class PostsController < ApplicationController
 
     @q = Post.ransack(params[:q])
     if params[:tag_name]
-      @posts = Post.tagged_with("#{params[:tag_name]}").includes(:tags).order(created_at: :desc).per(10)
+      @posts = Post.tagged_with("#{params[:tag_name]}").includes(:tags).order(created_at: :desc).page(params[:page]).per(10)
     elsif params[:q]
-      @posts = @q.result.order(created_at: :desc).per(10)
+      @posts = @q.result.order(created_at: :desc).page(params[:page]).per(10)
     else
       @posts = Post.includes(:user).all.order(created_at: :desc).page(params[:page]).per(10)
     end
