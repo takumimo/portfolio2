35cf62f8aaa16528ea3049dd1939da15a9a2a0f3
motokitakumi <mtktkm242424@gmail.com>
2020-12-17 21:03:24 +0900

    fix indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
     elsif params[:q]
       @posts = @q.result.order(created_at: :desc).page(params[:page]).per(10)
     else
-      @posts = Post.includes(:user).all.order(created_at: :desc).page(params[:page]).per(10)
+      @posts = Post.all.order(created_at: :desc).page(params[:page]).per(10)
     end
     @user = current_user
   end
