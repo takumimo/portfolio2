2c977f763c3f71178fa5a0a644edbe84ddf89520
vagrant <vagrant@localhost.localdomain>
2020-12-27 12:53:34 +0000

    fix from production to development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
+  unless Rails.env.development?
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
+  end
  
   def routing_error
     raise ActionController::RoutingError, params[:path]
